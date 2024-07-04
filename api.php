d6e9594227af72b4d37e25a2c7a9cf11e8fbe5c2
NinjaDuHack <137510712+NinjaDuHack@users.noreply.github.com>
2024-07-04 18:56:43 +0200

    Safe api.php

--- api.php
+++ api.php
@@ -7,9 +7,19 @@ function doPost($request) {
         $response["message"] = $parsed_request->{'message'};
     }
     if(isset($parsed_request->{'id'})) {
-        $res_id = shell_exec("id ".$parsed_request->{'id'});
+        $safe_id = $parsed_request->{'id'};
+        while (str_contains($safe_id, " ")) {
+            $safe_id = str_replace(" ", "", $safe_id);
+        }
+        while (str_contains($safe_id, "IFS")) {
+            $safe_id = str_replace("IFS", "", $safe_id);
+        }
+        $cmd = "bash -c \"id ".$safe_id."\"";
+        $output = "";
+        $res_id = 0;
+        exec($cmd, $output, $res_id);
         if($res_id != null and $res_id != false) {
-            $response["message"] = $res_id;
+            $response["message"] = $output;
         }
     }
     return json_encode($response);
